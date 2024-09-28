8e3683ac43192cef464e268a6329e206b748f780
Marcel Nunez <marcelpnunez@gmail.com>
2024-09-28 09:53:44 -0700

    Convert uniform random number to exponential

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,23 @@ use rand::Rng;
 fn main() {
     let mut rng = rand::thread_rng();
 
-    for i in 1..11 {
+    for _i in 1..11 {
         println!("Random float: {}", rng.gen::<f64>());
     }
+
+    let mut a = 100;
+    let mut t : f64 = 0.0;
+
+    println!("Time\tA pop");
+
+    println!("{t}\t{a}");
+    while a > 0 {
+        let r = rng.gen::<f64>();
+        let dt = -r.ln();
+        t += dt;
+        a-=1;
+        println!("{t}\t{a}");
+    }
+
     
 }
